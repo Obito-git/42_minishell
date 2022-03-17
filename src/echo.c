aaa1f3fb90959529977f1227b9f17067916477b3
cassepipe <cassepipe@ymail.com>
2022-03-17 10:23:49 +0100

    Added echo

---
+++ src/echo.c
@@ -0,0 +1,29 @@
+#include "minishell.h"
+
+int	echo(void *a)
+{
+	char **args;
+	bool newline;
+
+	newline = true;
+	args = (char**) a;
+	args++;
+	if (!ft_strcmp(*args, "-n"))
+	{
+		newline = false;
+		args++;
+	}
+	if (*args)
+	{
+		write(STDIN_FILENO, *args, ft_strlen(*args));
+		args++;
+	}
+	while (*args)
+	{
+		printf(" %s", *args);
+		args++;
+	}
+	if (newline)
+		printf("\n");
+	return (0);
+}
