1b2c72c4e699f741df52dbaf7c330c5f30ea4fb1
cassepipe <cassepipe@ymail.com>
2022-03-22 12:17:10 +0100

    Replaced STDIN by STDOUT in echo and pwd

--- src/echo.c
+++ src/echo.c
@@ -16,12 +16,12 @@ int	echo(t_command *cmd, t_strlist *env)
 	}
 	while (*args)
 	{
-		write(STDIN_FILENO, *args, ft_strlen(*args));
+		write(STDOUT_FILENO, *args, ft_strlen(*args));
 		if (*(args + 1))
-			write(STDIN_FILENO, " ", 1);
+			write(STDOUT_FILENO, " ", 1);
 		args++;
 	}
 	if (newline)
-		write(STDIN_FILENO, "\n", 1);
+		write(STDOUT_FILENO, "\n", 1);
 	return (0);
 }
