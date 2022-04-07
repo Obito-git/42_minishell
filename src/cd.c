733e9610a5060225fe4095e21f06afd4752817cd
Anton <amyroshn@Antons-MacBook-Pro.local>
2022-04-07 17:11:00 +0200

    cd + pwd fix + norminette

--- src/cd.c
+++ src/cd.c
@@ -11,35 +11,42 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
+int	cd_errors (char *path)
+{
+	int		ret;
+	char	cwd[PATH_MAX];
+
+	if (!getcwd(cwd, sizeof(cwd)))
+	{
+		perror("cd: error retrieving current directory: getcwd: cannot access parent directories");
+		return (0);
+	}
+	ret = chdir(path);
+	if (ret)
+	{
+		ft_dprintf_str(2,
+			"minishell: cd: %s: No such file or directory\n", path);
+		return (1);
+	}
+	return (ret);
+}
 
 int	cd(t_command *cmd, t_strlist *env)
 {
 	char	**paths;
-	int		ret;
 
 	(void) env;
 	paths = cmd->args;
 	paths++;
-	if (paths[0])
+	if (!paths[0])
 	{
-		if (paths[1])
-		{
-			ft_dprintf_str(2, "minishell: cd: too many arguments\n");
-			return (1);
-		}
-		else
-		{
-			ret = chdir(paths[0]);
-			if (ret)
-			{
-				ft_dprintf_str(2,
-					"minishell: cd: %s: No such file or directory\n", paths[0]);
-				return (1);
-			}
-			return (0);
-		}
-	}
-	else
 		ft_dprintf_str(2, "minishell: cd: you must provide a path\n");
-	return (1);
+		return (1);
+	}
+	if (paths[0] && paths[1])
+	{
+		ft_dprintf_str(2, "minishell: cd: too many arguments\n");
+		return (1);
+	}
+	return (cd_errors(paths[0]));
 }
--- src/pwd.c
+++ src/pwd.c
@@ -3,10 +3,13 @@
 int	pwd(t_command *cmd, t_strlist *env)
 {
 	char	cwd[PATH_MAX];
+	char	*res;
 
 	(void) cmd;
 	(void) env;
-	if (!getcwd(cwd, sizeof(cwd)))
+	getcwd(cwd, sizeof(cwd));
+	res = cwd;
+	if (!res)
 	{
 		perror("Unable to get filesystem path for current directory");
 		return (-1);
