a8aa6f3efd2228c6cf66725149942a44b3f86fa0
Anton <amyroshn@Antons-MacBook-Pro.local>
2022-04-07 12:26:43 +0200

    malloc

--- src/parsing.c
+++ src/parsing.c
@@ -69,9 +69,11 @@ t_strlist	*expand_pipeline(char *user_input, t_strlist *env)
 	t_strlist	*arg_list;
 
 	arg_list = new_strlist();
+	if (!arg_list)
+		return (NULL);
 	pipeline = split_on_unquoted_redir(user_input, ">|<");
 	i = 0;
-	while (pipeline[i])
+	while (pipeline && pipeline[i])
 	{
 		simple_cmd = expand_simple_command(pipeline[i], env);
 		if (!simple_cmd || append_strarray_to_strlist(arg_list, simple_cmd))
