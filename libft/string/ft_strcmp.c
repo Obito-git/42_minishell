dc4b1119bc10fc646e14dcddc4bfe3c4fdd68da9
Anton home <xd@xd.xd>
2022-04-12 11:39:35 +0200

    libft fixes

--- libft/string/ft_strcmp.c
+++ libft/string/ft_strcmp.c
@@ -20,6 +20,8 @@ int	ft_strcmp(char	*s1, char	*s2)
 		return (0);
 	if (!s1 || !s2)
 		return (1);
+	if (s1[i] != s2[i])
+		return (s1[i] - s2[i]);
 	while (s1[i] || s2[i])
 	{
 		if (s1[i] != s2[i])
